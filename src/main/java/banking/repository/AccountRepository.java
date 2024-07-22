27a463640bdd53ea4609908867bca564157972ce
Thabang <Thabang@DESKTOP-867BMT0>
2024-07-22 22:37:37 +0200

    Initial commit

---
+++ src/main/java/banking/repository/AccountRepository.java
@@ -0,0 +1,8 @@
+package banking.repository;
+
+
+import banking.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+}
